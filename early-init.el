6cbb2a959fa7b56a054a450e49b7bf919124db85
benevolent0505 <mikio.fjt@gmail.com>
2025-02-11 12:27:57 +0900

    early-init にパフォーマンス関係の設定追加

--- early-init.el
+++ early-init.el
@@ -3,9 +3,18 @@
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 
-(toggle-scroll-bar 0)
 (scroll-bar-mode -1)
 
 ;; GC
 ;; see: https://zenn.dev/takeokunn/articles/56010618502ccc#gc%E3%81%AE%E8%A8%AD%E5%AE%9A
-(setq gc-cons-threshold (* 128 1024 1024))
+;; see: https://apribase.net/2024/07/23/emacs-optimize/
+(setq gc-cons-threshold (* 128 1024 1024)
+      garbage-collection-messages nil)
+
+;; font
+;; see: https://apribase.net/2024/07/06/emacs-default-frame-alist/
+(add-to-list 'default-frame-alist '(font . "UDEV Gothic NF-19"))
+
+;; lsp-mode
+;; see: https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
+(setenv "LSP_USE_PLISTS" "true")
