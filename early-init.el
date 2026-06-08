60f48bc7d439e03eab98ded4cc49b8bfcf272cad
benevolent0505 <mikio.fjt@gmail.com>
2026-06-08 23:03:47 +0900

    frame settings

--- early-init.el
+++ early-init.el
@@ -6,6 +6,11 @@
 (scroll-bar-mode -1)
 (tab-bar-mode t)
 
+(setq default-frame-alist
+      (append '((width . 188)
+                (height . 56))
+              default-frame-alist))
+
 ;; GC
 ;; see: https://zenn.dev/takeokunn/articles/56010618502ccc#gc%E3%81%AE%E8%A8%AD%E5%AE%9A
 ;; see: https://apribase.net/2024/07/23/emacs-optimize/
